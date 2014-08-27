f9bc3b55576b3de260e246c5d0aa5c0a9556dac4
cesar abel <cesarabel@johnmay.com.ni>
2014-08-27 00:24:54 -0600

    fix Error importing request processor module rentas.easy_processors: "No module named easy_processors"

--- rentas/settings.py
+++ rentas/settings.py
@@ -133,6 +133,6 @@ import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 import django.conf.global_settings as DEFAULT_SETTINGS
 
-CUSTOM_PROCESSORS = ('rentas.easy_processors.applist',)
+CUSTOM_PROCESSORS = ('recibos.easy_processors.applist',)
 
 TEMPLATE_CONTEXT_PROCESSORS = DEFAULT_SETTINGS.TEMPLATE_CONTEXT_PROCESSORS + CUSTOM_PROCESSORS
