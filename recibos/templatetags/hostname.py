f7df1f471cd525aa79d8d2721b2e717f03d9d931
cesar abel <cesarabel@johnmay.com.ni>
2014-09-13 17:31:02 -0600

    template tags

---
+++ recibos/templatetags/hostname.py
@@ -0,0 +1,9 @@
+from django.contrib.sites.models import Site
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def get_hostname():
+    current_site = Site.objects.get_current()
+    return current_site.domain
---
+++ rentas/static/admin/js/extra.js
@@ -0,0 +1,3 @@
+function ocultar {
+	
+}
