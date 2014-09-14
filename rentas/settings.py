34e1f3563297a2adb4d62a3cee8a2d3c307f72bd
cesar abel <cesarabel@johnmay.com.ni>
2014-09-13 22:44:04 -0600

    Estilo index

--- rentas/settings.py
+++ rentas/settings.py
@@ -12,11 +12,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'rentas',                      # Or path to database file if using sqlite3.
+        'NAME': 'dd0lkll4l2717a',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'rsujiklxazuzbo',
+        'PASSWORD': 'abxYG-a0IR84FV-7zL57a1BgP4',
+        'HOST': 'ec2-54-235-83-5.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                      # Set to empty string for default.
     }
 }
@@ -130,7 +130,7 @@ LOGGING = {
 }
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 import django.conf.global_settings as DEFAULT_SETTINGS
 
 CUSTOM_PROCESSORS = ('recibos.easy_processors.applist',)
