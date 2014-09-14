32d22af9e21df60269e726e0b6e50179970364e4
cesar abel <cesarabel@johnmay.com.ni>
2014-09-13 18:18:31 -0600

    template tags estadisticas

--- recibos/templatetags/modulo.py
+++ recibos/templatetags/modulo.py
@@ -3,10 +3,30 @@ from django import template
 
 register = template.Library()
 
+class estadisticasNode(template.Node):
+    def __init__(self, varname):
+        self.varname
+
+    def __repr__(self):
+        return "<GetEstadisticas Node>"
+
+    def render(self, context):
+            context[self.varname] = Recibo.objects.filter(periodo__in=Periodo.objects.filter(cerrado=False))
+            return ''
+
 @register.tag
-def get_estadisticas():
-    recibos = Recibo.objects.filter(periodo__in=Periodo.objects.filter(cerrado=False))
-    if recibos:
-        return recibos
-    else:
-        return None
+def get_estadisticas(parser, token):
+    """
+        uso
+            {% get_estadisticas as [varname]%}
+    """
+    tokens = token.contents.split()
+    
+    if not len(tokens) == 2:
+        raise template.TemplateSyntaxError(
+            "'get_estadisticas' requiere de dos argumentos")
+    if not tokens[2]=='as':
+        raise template.TemplateSyntaxError(
+            "'get_estadisticas' requiere que el primer argumento se 'as'")
+        
+    return estadisticasNode(varname=tokens[2])
