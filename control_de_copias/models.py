b39fff19ecdbe21d93d37d4c3b78234479a60171
cesar abel <cesarabel@johnmay.com.ni>
2014-09-20 14:50:51 -0600

    manager recibos

--- control_de_copias/models.py
+++ control_de_copias/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class recibo_manager(models.Manager):
     def get_queryset(self):
-        return super(recibo_manager,self).get_queryset().filter(periodo__in=Periodo.objects.filter(cerrado=False))
+        return super(recibo_manager,self).get_queryset().filter(periodo__in=Periodo.objects.filter(cerrado=False).values_list('id',flat=True).order_by('id'))
 
 class recibo(Recibo):
     objects = models.Manager()
