a01e609baf0ac87c790c8f0369250d83cb7521c5
cesar abel <cesarabel@johnmay.com.ni>
2014-09-20 15:14:31 -0600

    manager

--- control_de_copias/models.py
+++ control_de_copias/models.py
@@ -2,9 +2,9 @@ from recibos.models import Recibo,Periodo
 from django.db import models
 
 class recibo_manager(models.Manager):
-    def get_queryset(self):
+    def get_query_set(self):
         periodos = Periodo.objects.filter(cerrado=False)
-        return super(recibo_manager,self).get_queryset().all().filter(periodo__in=periodos)
+        return super(recibo_manager,self).get_queryset().filter(periodo__in=periodos)
 
 class recibo(Recibo):
     objects = models.Manager()
