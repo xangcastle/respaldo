4d921eb336d71f920768effc53149683f6932594
cesar abel <cesarabel@johnmay.com.ni>
2014-10-02 22:07:01 -0600

    existencias

--- recibos/models.py
+++ recibos/models.py
@@ -257,6 +257,10 @@ class Articulo(models.Model):
             return 0
     total_salidas.allow_tags = True
     
+    def existencias(self):
+        return self.total_entradas() - self.total_salidas()
+    existencias.allow_tags = True
+    
 class EntradaManager(models.Manager):
     def get_query_set(self):
         return super(EntradaManager,self).get_query_set().filter(tipo_requisa='EN')
