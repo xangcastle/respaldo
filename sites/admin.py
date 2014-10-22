34b41029baff980993a31c3a03435c2f7ad5ceca
xangmuve <cesarabel@johnmay.com.ni>
2014-10-22 01:22:45 -0600

    prueba de filtro de equipos

--- sites/admin.py
+++ sites/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+from recibos.models import Site
 from models import site_equipo,site_inventario,site_requisa
+
+class site_equipo_admin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        sds = Site.objects.filter(encargado=request.user)
+        le = []
+        if sds:
+            for s in sds:
+                le += s.equipos.values_list('id',flat=True)
+        ids = site_equipo.objects.filter(id__in=le).values_list('id',flat=True)
+        qs = super(site_equipo_admin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(id__in=ids)
 
 admin.site.register(site_equipo)
 admin.site.register(site_inventario)
