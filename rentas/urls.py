2c5193dba3632654fe09598cb6d5368add454d41
xangmuve <cesarabel@johnmay.com.ni>
2014-10-21 22:52:37 -0600

    grappelli urls

--- rentas/urls.py
+++ rentas/urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$','recibos.views.principal',name='principal'),
+    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rentas/', include('recibos.urls')),
