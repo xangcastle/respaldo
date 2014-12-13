6b3f9590c95757d487d3a4983afd81369dede6ad
xangmuve <cesarabel@johnmay.com.ni>
2014-12-13 17:02:01 -0600

    moneycash

---
+++ moneycash/migrations/0002_auto_20141213_1702.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('moneycash', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='periodo',
+            name='activo',
+        ),
+        migrations.RemoveField(
+            model_name='periodo',
+            name='code',
+        ),
+        migrations.RemoveField(
+            model_name='periodo',
+            name='name',
+        ),
+    ]
