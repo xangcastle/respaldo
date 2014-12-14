940436fc0faf7776bdc3bb3fd221da45fc2a70c0
xangmuve <cesarabel@johnmay.com.ni>
2014-12-13 18:12:26 -0600

    moneycash

--- actualizacion.sh
+++ actualizacion.sh
@@ -4,5 +4,6 @@ echo "Actualizando aplicacion en servidor heroku"
 echo
 python manage.py makemigrations &&
 echo 
-git add . --all && git commit -am $1 && git push heroku master && heroku run python manage.py migrate
+git add . --all && git commit -am $1 && git push heroku master && heroku run python manage.py migrate && python manage.py fix_permisions
+
 echo "Gracias Cesar Abel"
