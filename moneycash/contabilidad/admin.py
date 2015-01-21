784724bcf963dfa88a33d56db94eefc030e9376a
xangmuve <cesarabel@johnmay.com.ni>
2015-01-20 22:39:50 -0600

    bancos

--- moneycash/contabilidad/admin.py
+++ moneycash/contabilidad/admin.py
@@ -1,18 +1,9 @@
-from moneycash.admin import admin, documento_admin
-from moneycash.contabilidad.models import Poliza, Factura#, Producto
+from moneycash.admin import admin, entidad_admin
+from .models import Banco
 
 
-class poliza_factura(admin.TabularInline):
-    model = Factura
-
-
-#class poliza_producto(admin.TabularInline):
-    #model = Producto
-
-
-class poliza_admin(documento_admin):
-    #inlines = [poliza_factura, poliza_producto]
+class banco_admin(entidad_admin):
     pass
 
 
-admin.site.register(Poliza, poliza_admin)
+admin.site.register(Banco, banco_admin)
--- moneycash/contabilidad/models.py
+++ moneycash/contabilidad/models.py
@@ -1,20 +1,7 @@
-from moneycash.models import Poliza as base_poliza,\
-DetallePoliza as base_detalle_poliza#, poliza_producto as base_producto
+from moneycash.models import Banco as base_banco
 
 
-class Factura(base_detalle_poliza):
+class Banco(base_banco):
     class Meta:
         proxy = True
-        verbose_name_plural = "facturas incluidas en la poliza"
 
-
-#class Producto(base_producto):
-    #class Meta:
-        #proxy = True
-        #verbose_name_plural = "productos incluidos en la poliza"
-
-
-class Poliza(base_poliza):
-    class Meta:
-        proxy = True
-        verbose_name_plural = "liquidacion de polizas"
