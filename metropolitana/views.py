2d7b69333cdbdec874b4854328d987d02c2eebf4
xangmuve <cesarabel@johnmay.com.ni>
2015-05-05 20:21:11 -0600

    fix error import ho.pisa

--- metropolitana/views.py
+++ metropolitana/views.py
@@ -34,27 +34,27 @@ def datos_paquete(request):
 
 
 ########################################################
-import ho.pisa as pisa
-import cStringIO as StringIO
-import cgi
-from django.template import RequestContext
-from django.template.loader import render_to_string
+#import ho.pisa as pisa
+#import cStringIO as StringIO
+#import cgi
+#from django.template import RequestContext
+#from django.template.loader import render_to_string
 
 
-def generar_pdf(html):
-    result = StringIO.StringIO()
-    pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("UTF-8")), result)
-    if not pdf.err:
-        return HttpResponse(result.getvalue(), content_type='application/pdf')
-    return HttpResponse('Error al generar el PDF: %s' % cgi.escape(html))
+#def generar_pdf(html):
+    #result = StringIO.StringIO()
+    #pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("UTF-8")), result)
+    #if not pdf.err:
+        #return HttpResponse(result.getvalue(), content_type='application/pdf')
+    #return HttpResponse('Error al generar el PDF: %s' % cgi.escape(html))
 
 
-def comprobantes_pdf(request, id):
-    queryset = Lote.objects.filter(id__in=id)
-    html = render_to_string('metropolitana/comprobante.html',
-        {'pagesize': 'A4', 'queryset': queryset},
-        context_instance=RequestContext(request))
-    return generar_pdf(html)
+#def comprobantes_pdf(request, id):
+    #queryset = Lote.objects.filter(id__in=id)
+    #html = render_to_string('metropolitana/comprobante.html',
+        #{'pagesize': 'A4', 'queryset': queryset},
+        #context_instance=RequestContext(request))
+    #return generar_pdf(html)
 
 
 
